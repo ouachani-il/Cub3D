3aa4f0a1a73d1fe04ef21382c634c191ded2d547
Ilham OUACHANI <ilouacha@bess-f1r6s2.clusters.42paris.fr>
2024-02-28 10:59:14 +0100

    data structure definition

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 18:28:00 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/02/27 18:29:58 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/02/28 10:31:34 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,54 @@
 # define CUB3D_H
 
 # include "mlx/mlx.h"
+# include <unistd.h>
 
-#define struct 
+typedef struct s_vector
+{
+	double		x;
+	double		y;
+}				t_vector;
+
+typedef struct s_mlx
+{
+	void		*mlx_ptr;
+	void		*mlx_win;
+}				t_mlx;
+
+typedef struct s_ray
+{
+	t_vector	pos;
+	t_vector	dir;
+	t_vector	plane;
+	t_vector	ray_dir;
+	int			step_x;
+	int			step_y;
+	t_vector	side_dist;
+	t_vector	delta_dist;
+	double		move_speed;
+	double		rot_spped;
+	int			texture;
+}				t_ray;
+
+typedef struct s_data
+{
+	char	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+	char	**fd_tab;
+	int		nbr_line;
+	int		ceil_rgb[3];
+	int		floor_rgb[3];
+	int		height;
+	int		width;
+	int		dx;
+	int		dy;
+	int		r;
+	int		g;
+	int		b;
+	int		pos_x;
+	int		pos_y;
+}			t_data;
+
 #endif
--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: ilouacha <ilouacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 13:46:31 by ilouacha          #+#    #+#             */
-/*   Updated: 2024/02/27 15:12:32 by ilouacha         ###   ########.fr       */
+/*   Updated: 2024/02/28 10:31:11 by ilouacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx/mlx.h"
-#include <unistd.h>
+#include "cub3d.h"
 
 void	ft_putchar(char c)
 {
@@ -23,18 +23,22 @@ int	deal_key(int key, void *param)
 	return (0);
 }
 
-int	main()
+int	main(int ac, char **av)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 800, 500, "Cub3D");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-	mlx_key_hook(win_ptr, deal_key, (void *)0);
-	// mlx_key_hook(win_ptr, int (*func_ptr)(), void *param);
-	// mlx_mouse_hook(win_ptr, int (*func_ptr)(), void *param);
-	// mlx_expose_hook(win_ptr, int (*func_ptr)(), void *param);
-	mlx_loop(mlx_ptr);
+	t_data		*data;
 	
+	if (ac == 2)
+	{
+		mlx_ptr = mlx_init();
+		win_ptr = mlx_new_window(mlx_ptr, 800, 500, "Cub3D");
+		mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+		mlx_key_hook(win_ptr, deal_key, (void *)0);
+		// mlx_key_hook(win_ptr, int (*func_ptr)(), void *param);
+		// mlx_mouse_hook(win_ptr, int (*func_ptr)(), void *param);
+		// mlx_expose_hook(win_ptr, int (*func_ptr)(), void *param);
+		mlx_loop(mlx_ptr);
+	}
+	return (0);
 }
